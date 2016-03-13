09aaa228728eaab0c326d1889957fe0b5d11d534
Steve <scantwell4@gmail.com>
2016-03-13 18:35:33 -0400

    Merged with master

--- app/src/main/java/com/drexelsp/blunote/blunote/Service.java
+++ app/src/main/java/com/drexelsp/blunote/blunote/Service.java
@@ -71,7 +71,7 @@ public class Service extends ClientService {
             super.send(pdu.toByteArray());
         }
     }
-    
+
     public void onCreate() {
         super.onCreate();
         this.handlers.add(new Metadata(getApplicationContext()));
