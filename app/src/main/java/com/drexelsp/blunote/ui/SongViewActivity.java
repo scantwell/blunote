5eb2c0c7a1a53342d3343b46d84f4e165275bf63
Matt Higgins <xm1337@gmail.com>
2016-03-13 22:10:29 -0400

    SongViewActivity now populates Title, Album, and Artist for the selected song

--- app/src/main/java/com/drexelsp/blunote/ui/SongViewActivity.java
+++ app/src/main/java/com/drexelsp/blunote/ui/SongViewActivity.java
@@ -1,18 +1,24 @@
 package com.drexelsp.blunote.ui;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.drexelsp.blunote.blunote.Constants;
 import com.drexelsp.blunote.blunote.R;
+import com.drexelsp.blunote.provider.MetaStoreContract;
 
 /**
  * Created by U6020377 on 1/25/2016.
  */
 public class SongViewActivity extends BaseBluNoteActivity implements View.OnClickListener {
+    private static final String TAG = "Song View Activity";
     TextView songViewTitle;
     TextView songViewArtist;
     TextView songViewAlbum;
@@ -30,6 +36,8 @@ public class SongViewActivity extends BaseBluNoteActivity implements View.OnClic
         songViewOwner = (TextView) findViewById(R.id.song_view_owner);
 
         song_view_add_to_queue.setOnClickListener(this);
+
+        populateSongDetails();
     }
 
     @Override
@@ -58,6 +66,26 @@ public class SongViewActivity extends BaseBluNoteActivity implements View.OnClic
     }
 
     public void populateSongDetails() {
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("_id");
+        Uri uri = Uri.withAppendedPath(MetaStoreContract.Track.CONTENT_URI, id);
+        Log.v(TAG, uri.toString());
+        String[] selection = { "title", "artist", "album" };
+        Cursor cursor = getContentResolver().query(uri, selection, null, null, null);
+
+        if (cursor != null && cursor.moveToNext()) {
+            Log.v(TAG, "Results found");
+            String title = cursor.getString(cursor.getColumnIndex("title"));
+            songViewTitle.setText(title);
+            String artist = cursor.getString(cursor.getColumnIndex("artist"));
+            songViewArtist.setText(artist);
+            String album = cursor.getString(cursor.getColumnIndex("album"));
+            songViewAlbum.setText(album);
+            String owner = "SongOwner";
+            songViewOwner.setText(owner);
+            Log.v(TAG, String.format("Title: %s, Artist: %s, Album: %s, Owner: %s", title, artist, album, owner));
+            cursor.close();
+        }
 
     }
 }
