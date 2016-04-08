7ba281c3990cdb1225deb727e299f41f2a99658e
Matt Higgins <xm1337@gmail.com>
2016-04-08 01:12:25 -0400

    Added android-style settings page to implement SharedPreferences.
    Combined settings and network preferences into preferences.

---
+++ app/src/main/java/com/drexelsp/blunote/ui/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.drexelsp.blunote.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Settings Activity
+ * Doesn't need to do more than just setup the SettingsFragment
+ *
+ * Created by omnia on 4/7/16.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
