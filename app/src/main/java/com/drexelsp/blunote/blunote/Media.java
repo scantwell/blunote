8b8cb69fd36358c3e00c29d7e69e0522ada1f346
Michael Brisbin <michael.brisbin@thomsonreuters.com>
2016-04-14 18:28:16 -0400

    Fixed up a default username.

--- app/src/main/java/com/drexelsp/blunote/blunote/Media.java
+++ app/src/main/java/com/drexelsp/blunote/blunote/Media.java
@@ -1,9 +1,9 @@
 package com.drexelsp.blunote.blunote;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
@@ -43,7 +43,8 @@ public class Media implements MessageHandler {
     private Player player;
 
     public Media(Context context, Service service) {
-        this.username = PreferenceManager.getDefaultSharedPreferences(context).getString("pref_key_user_name", "username");
+        this.username = PreferenceManager.getDefaultSharedPreferences(context).getString(
+                "pref_key_user_name", BluetoothAdapter.getDefaultAdapter().getName());
         this.mService = service;
         this.mContentResolver = context.getContentResolver();
         this.cacheDir = context.getCacheDir();
