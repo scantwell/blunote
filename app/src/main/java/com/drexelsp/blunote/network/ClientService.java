39e7f4b8c7cffef3387c3386c41ee1c62fd3f439
Stephen Cantwell <scantwell4@gmail.com>
2016-04-27 16:03:30 -0400

    Trying to fix weird upstream/downstream clienthandler bug

--- app/src/main/java/com/drexelsp/blunote/network/ClientService.java
+++ app/src/main/java/com/drexelsp/blunote/network/ClientService.java
@@ -25,9 +25,7 @@ abstract public class ClientService extends Service {
     abstract public void onNetworkEvent(BluetoothEvent bluetoothEvent);
 
     // Sends to another application via bluetooth/etc
-    protected void sendUpstream(byte[] data) {
-        this.send(ClientHandler.SEND_UPSTREAM, data);
-    }
+    protected void sendUpstream(byte[] data) { this.send(ClientHandler.SEND_UPSTREAM, data); }
 
     // Sends to another application via bluetooth/etc
     protected void sendDownstream(byte[] data) {
@@ -37,7 +35,16 @@ abstract public class ClientService extends Service {
     // Sends to another application via bluetooth/etc
     private void send(int direction, byte[] data) {
         Log.v(TAG, "Sending message.");
-        Message msg = Message.obtain(null, direction, 0, 0);
+        Message msg;
+        if ( direction == ClientHandler.SEND_DOWNSTREAM )
+        {
+            msg = Message.obtain(null, ClientHandler.SEND_DOWNSTREAM, 0, 0);
+        }
+        else
+        {
+            msg = Message.obtain(null, ClientHandler.SEND_UPSTREAM, 0, 0);
+        }
+
         Bundle bundle = new Bundle(1);
         bundle.putByteArray("data", data);
         msg.setData(bundle);
