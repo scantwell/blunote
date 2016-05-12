62bde428e49989ac093bb1882f0edd4a71783a2d
Steve <scantwell4@gmail.com>
2016-05-12 18:35:52 -0400

    Add/remove songs from playlist activity

--- app/src/main/java/com/drexelsp/blunote/ui/PlaylistActivity.java
+++ app/src/main/java/com/drexelsp/blunote/ui/PlaylistActivity.java
@@ -37,26 +37,10 @@ public class PlaylistActivity extends BaseBluNoteActivity implements ListView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!Intent.ACTION_SEARCH.equals(getIntent().getAction())) {
-            getCurrentPlaylist();
-            playlistView = (ListView) findViewById(R.id.playlist_list);
-            setSimpleList(playlistView, playlist);
-            registerForContextMenu(playlistView);
-        }
+        playlistView = (ListView) findViewById(R.id.playlist_list);
         this.playlist = new ArrayList<>();
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        if (v.getId() == R.id.playlist_list) {
-            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-            menu.setHeaderTitle(playlist.get(info.position));
-
-            String[] menuItems = getResources().getStringArray(R.array.playlist_context_array);
-            for (int i = 0; i < menuItems.length; i++) {
-                menu.add(Menu.NONE, i, i, menuItems[i]);
-            }
-        }
+        setSimpleList(playlistView, playlist);
+        registerForContextMenu(playlistView);
     }
 
     @Override
@@ -79,38 +63,6 @@ public class PlaylistActivity extends BaseBluNoteActivity implements ListView.On
         return true;
     }
 
-    @Override
-    public void handleIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            playlistView = (ListView) findViewById(R.id.playlist_list);
-            //use the query to search your data somehow
-            List<String> connectionList = getCurrentPlaylist();
-            Iterator<String> i = connectionList.iterator();
-            while (i.hasNext()) {
-                String item = i.next();
-                if (!item.contains(query)) {
-                    i.remove();
-                }
-            }
-
-            registerForContextMenu(playlistView);
-            playlistAdapter = new ArrayAdapter(this,
-                    android.R.layout.simple_list_item_1, playlist);
-            playlistView.setAdapter(playlistAdapter);
-            SearchView.OnCloseListener closeListener = new SearchView.OnCloseListener() {
-                @Override
-                public boolean onClose() {
-                    playlistAdapter = new ArrayAdapter(playlistAdapter.getContext(),
-                            android.R.layout.simple_list_item_1, getCurrentPlaylist());
-                    playlistView.setAdapter(playlistAdapter);
-                    return false;
-                }
-            };
-            searchView.setOnCloseListener(closeListener);
-        }
-    }
-
     public List<String> getCurrentPlaylist() {
         return this.playlist;
     }
@@ -123,11 +75,13 @@ public class PlaylistActivity extends BaseBluNoteActivity implements ListView.On
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     public void onAddSongEvent(AddSongEvent event) {
         this.playlist.add(String.format("Song: %s Artist: %s Album:%", event.title, event.artist, event.album));
+        this.playlistAdapter.notifyDataSetChanged();
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     public void onPlaySong(PlaySongEvent event) {
         removeSong(event.title);
+        this.playlistAdapter.notifyDataSetChanged();
     }
 
     private void removeSong(String title) {
